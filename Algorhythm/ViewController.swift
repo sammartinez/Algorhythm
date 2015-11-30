14d9a7a8c9ef3f8ede357318738c7d628c0505cc
Sam Martinez <sammartinez19@gmail.com>
2015-11-29 20:41:32 -0800

    Initial Commit

---
+++ Algorhythm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Algorhythm
+//
+//  Created by Sam Martinez  on 11/29/15.
+//  Copyright © 2015 Sam Martinez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
