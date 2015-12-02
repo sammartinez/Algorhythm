89a60f0bd887c250ea0505f4d034006defa52ff5
Sam Martinez <sammartinez19@gmail.com>
2015-12-01 23:50:45 -0800

    first commit

---
+++ Algorhythm/Playlist.swift
@@ -0,0 +1,9 @@
+//
+//  Playlist.swift
+//  Algorhythm
+//
+//  Created by Sam Martinez  on 12/1/15.
+//  Copyright © 2015 Sam Martinez . All rights reserved.
+//
+
+import Foundation
---
+++ Algorhythm/PlaylistDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaylistDetailViewController.swift
+//  Algorhythm
+//
+//  Created by Sam Martinez  on 11/29/15.
+//  Copyright © 2015 Sam Martinez . All rights reserved.
+//
+
+import UIKit
+
+class PlaylistDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
